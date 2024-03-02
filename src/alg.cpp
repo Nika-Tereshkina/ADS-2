7abf390eaa9a098fde24903c4d2d9f898a1811d4
Nika-Tereshkina <159996107+Nika-Tereshkina@users.noreply.github.com>
2024-03-03 00:40:05 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,45 @@
 
 
 double pown(double value, uint16_t n) {
-
+    double res = 1.0;
+    for (uint16_t i = 0; i < n; i++) {
+        res *= value;
+        }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint64_t res = 1;
+    for (uint16_t i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    double summa = 1.0;
+    for (uint16_t n = 1; n < count; n++) {
+        summa += calcItem(x, n);
+    }
+    return summa;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double summa = x;
+    for (uint16_t n = 2; n < count; n += 2) {
+        summa += (n % 4 == 1 ? 1 : -1) * calcItem(x, n);
+    }
+    return summa;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double summa = 1.0;
+    for (uint16_t n = 2; n < count; n += 2) {
+        summa += (n % 4 == 0 ? 1 : -1) * calcItem(x, n);
+    }
+    return summa;
 }
